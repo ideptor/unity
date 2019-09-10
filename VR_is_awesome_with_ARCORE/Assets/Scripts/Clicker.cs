486a3d258991db038515db973735f7c778d93d1d
Suhyun Kim <ideptor@gmail.com>
2019-09-10 14:44:04 +0900

    Ethan walks towards to WalkingTarget

---
+++ VR_is_awesome_with_ARCORE/Assets/Scripts/Clicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//public class Clicker : MonoBehaviour
+public class Clicker
+{
+
+    public bool clicked()
+    {
+#if (UNITY_ANDROID || UNITY_IPHONE)
+        //return Cardboard.SDK.CardBoardTriggered;
+#else
+        //return Input.anyKeyDown;
+#endif
+        return Input.anyKeyDown;
+    }
+}
