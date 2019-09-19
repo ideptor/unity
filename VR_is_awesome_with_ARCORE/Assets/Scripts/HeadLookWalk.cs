a495f9c7c987e43537a0608ac7f5c646c7d23640
Suhyun Kim <ideptor@gmail.com>
2019-09-19 17:23:34 +0900

    see for first view, and walking

--- VR_is_awesome_with_ARCORE/Assets/Scripts/HeadLookWalk.cs
+++ VR_is_awesome_with_ARCORE/Assets/Scripts/HeadLookWalk.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class HeadLookWalk : MonoBehaviour
 {
+
+    public float velocity = 0.7f;
+    public Transform arCamera;
+    private CharacterController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        Vector3 moveDirection = arCamera.transform.forward;
+        moveDirection *= velocity * Time.deltaTime;
+        moveDirection.y = 0.0f;
+        controller.Move(moveDirection);
+        */
+        controller.SimpleMove(arCamera.forward * velocity);
     }
 }
