c806eb7c1a2875114e26dc3bddd20d67f701381b
Suhyun Kim <ideptor@gmail.com>
2019-09-19 11:34:30 +0900

    start stop button

---
+++ VR_is_awesome_with_ARCORE/Assets/Scripts/ButtonExecute.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonExecute : MonoBehaviour
+{
+    public Transform camera;
+
+    private GameObject currentButton;
+    private Clicker clicker = new Clicker();
+
+    private float timeToSelect = 1.0f;
+    private float countDown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        RaycastHit hit;
+        GameObject hitButton = null;
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.gameObject.tag == "Button")
+            {
+                hitButton = hit.transform.parent.gameObject;
+            }
+        }
+        if(currentButton != hitButton)
+        {
+            if(currentButton != null)
+            {
+                ExecuteEvents.Execute<IPointerExitHandler>(currentButton, data, ExecuteEvents.pointerExitHandler);
+            }
+            currentButton = hitButton;
+            if(currentButton != null)
+            {
+                ExecuteEvents.Execute<IPointerEnterHandler>(currentButton, data, ExecuteEvents.pointerEnterHandler);
+                countDown = timeToSelect;
+            }
+        }
+        if(currentButton != null)
+        {
+            countDown -= Time.deltaTime;
+            if(clicker.clicked() || countDown < 0.0f)
+            {
+                ExecuteEvents.Execute<IPointerClickHandler>(currentButton, data, ExecuteEvents.pointerClickHandler);
+                countDown = timeToSelect;
+            }
+        }
+    }
+}
