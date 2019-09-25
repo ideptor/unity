0a9a73feaa8e07e3be0a7616ff43d054c81d8d23
Suhyun Kim <ideptor@gmail.com>
2019-09-25 16:21:12 +0900

    Video play/stop

---
+++ UnityFeasibilityTest/Assets/Scripts/VideoBtnController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoBtnController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        Debug.Log("OnPlayButtonClicked() - ");
+        var video = FindObjectOfType<VideoController>();
+        video.PlayVideo();
+    }
+
+    public void OnPauseButtonClicked()
+    {
+        Debug.Log("OnPauseButtonClicked() - ");
+        var video = FindObjectOfType<VideoController>();
+        video.PauseVideo();
+    }
+}
---
+++ UnityFeasibilityTest/Assets/Scripts/VideoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+   // public RawImage Image;
+    VideoPlayer video;
+    //AudioSource audio;
+
+    //Boolean isPaused = false;
+    Boolean isStarted = false;
+
+    void Awake()
+    {
+        //Image = GetComponent<RawImage>();
+        video = FindObjectOfType<VideoPlayer>();
+        //audio = gameObject.AddComponent<AudioSource>();
+        video.playOnAwake = false;
+        //audio.playOnAwake = false;
+        //audio.Pause();
+        StartCoroutine(Prepare());
+        //PlayVideo();
+
+    }
+
+    public void PlayVideo()
+    {
+        if(!isStarted)
+        {
+            StartCoroutine(playVideo());
+            isStarted = true;
+        } else
+        {
+            video.Play();
+        }
+        
+    }
+
+    internal void PauseVideo()
+    {
+        if(isStarted)
+        {
+            video.Pause();
+        }
+    }
+
+    private IEnumerator Prepare()
+    {
+        video.source = VideoSource.Url;
+        video.url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        video.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        video.EnableAudioTrack(0, true);
+        //video.SetTargetAudioSource(0, audio);
+        video.Prepare();
+        WaitForSeconds waitTime = new WaitForSeconds(1.0f);
+
+        while (!video.isPrepared)
+        {
+            Debug.Log("동영상 준비중...");
+            yield return waitTime;
+        }
+
+        Debug.Log("동영상이 준비가 끝났습니다.");
+    }
+
+    IEnumerator playVideo()
+    {
+
+
+        //Image.texture = video.texture;
+        video.Play();
+        //audio.Play();
+
+        Debug.Log("동영상이 재생됩니다.");
+
+        while (video.time < video.length)
+        {
+            Debug.Log("동영상 재생 시간 : " + Mathf.FloorToInt((float)video.time) + "/" + Mathf.FloorToInt((float)video.length));
+            yield return null;
+        }
+        Debug.Log("영상이 끝났습니다.");
+
+    }
+}
