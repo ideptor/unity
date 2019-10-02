b3aa76119e2d3c462a7094e63ef34805401259dd
Suhyun Kim <ideptor@gmail.com>
2019-10-02 09:34:59 +0900

    smoth move for compass update

---
+++ FoodyGo/Assets/Controllers/CharacterGPSCompassController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGPSCompassController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.compass.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var heading = 180 + Input.compass.magneticHeading;
+        var rotation = Quaternion.AngleAxis(heading, Vector3.up);
+        //transform.rotation = rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.fixedTime * .001f);
+    }
+}
