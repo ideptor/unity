707e9fd3ea82dd14b01f82e722812d46e41d7672
Suhyun Kim <ideptor@gmail.com>
2019-10-02 11:55:10 +0900

    add monster service & distance indicator

---
+++ FoodyGo/Assets/FoodyGo/Scripts/Mapping/MathG.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace packt.FoodyGO.Mapping
+{
+    public static class MathG
+    {
+        //public static float Distance(MapLocation mp1, MapLocation mp2)
+        public static float Distance(double lon1, double lat1, double lon2, double lat2)
+        {
+            double R = 6371;        // 지구의 평균반경 km값
+
+            // double로 변환하여 정확도를 높이고 반올림 오류를 피한다.
+            /*
+            double lat1 = mp1.Latitude;
+            double lon1 = mp1.Longitude;
+
+            double lat2 = mp2.Latitude;
+            double lon2 = mp2.Longitude;
+            */
+
+            lat1 = deg2rad(lat1);
+            lon1 = deg2rad(lon1);
+            lat2 = deg2rad(lat2);
+            lon2 = deg2rad(lon2);
+
+            var dlat = (lat2 - lat1);
+            var dlon = (lon2 - lon1);
+
+            var a = Math.Pow(Math.Sin(dlat / 2), 2) + Math.Cos(lat1) *
+                Math.Cos(lat2) * Math.Pow(Math.Sin(dlon / 2), 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = c * R;
+
+            return (float)d * 1000;
+        }
+
+        public static double deg2rad(double deg)
+        {
+            var rad = deg * Math.PI / 180;
+            // 라디안 = 각도 * 파이 / 180
+
+            return rad;
+        }
+    }
+
+
+
+}
---
+++ FoodyGo/Assets/FoodyGo/Scripts/Services/MonsterService.cs
@@ -0,0 +1,50 @@
+﻿using packt.FoodyGO.Mapping;
+using packt.FoodyGO.Services;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace packt.FoodyGo.Services
+{
+    public class MonsterService : MonoBehaviour
+    {
+
+        public GPSLocationService gpsLocationService;
+        public Text distanceText;
+
+        private double lastTimestamp;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(gpsLocationService != null && 
+                gpsLocationService.IsServiceStarted &&
+                gpsLocationService.PlayerTimestamp > lastTimestamp)
+            {
+                lastTimestamp = gpsLocationService.PlayerTimestamp;
+                var s = MathG.Distance(
+                    gpsLocationService.Longitude,
+                    gpsLocationService.Latitude,
+                    gpsLocationService.mapCenter.Longitude,
+                    gpsLocationService.mapCenter.Latitude
+                    );
+
+                Debug.Log("Play distance from map tile center = " + s);
+                if(distanceText != null)
+                {
+                    distanceText.text = string.Format("L:({0}, {1})\nD:{2}",
+                        gpsLocationService.Longitude.ToString("F3"),
+                        gpsLocationService.Latitude.ToString("F3"),
+                        s.ToString("F8"));
+                }
+
+            }
+        }
+    }
+
+}
